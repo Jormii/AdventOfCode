3bf7de32db896ccc645c8c68e63fdde096d148b1
Jorge Lopez <lopeznatalj@gmail.com>
2024-12-01 20:35:27 +0100

    Updated profile_py.py

--- profile_py.py
+++ profile_py.py
@@ -10,13 +10,19 @@ EXECUTIONS = 100
 assert os.name == 'posix'
 
 print(f'--- {DAY} / {YEAR} ---')
-os.chdir(os.path.join(f'{YEAR}', f'{DAY:02}'))
 
 for part in [1, 2]:
-    PATTERN = r'[\.\d]+'
-    RUN_ARGS = ['/usr/bin/time', '-f', '"%e3"', 'python', f'part{part}.py']
+    PATTERN = rf'([.0-9]+) part{part}.py:\d+\(main\)'
+    RUN_ARGS = [
+        'python',
+        '-m',
+        'cProfile',
+        '-s',
+        'cumtime',
+        f'{YEAR}/{DAY:02}/part{part}.py'
+    ]
 
-    print(f'Part {part}: ', end='')
+    print(f'Part {part} ({" ".join(RUN_ARGS)}): ', end='')
 
     process = subprocess.run(RUN_ARGS, stdout=PIPE, stderr=PIPE)
     if process.returncode != 0:
@@ -27,7 +33,10 @@ for part in [1, 2]:
     for i in range(EXECUTIONS):
         process = subprocess.run(RUN_ARGS, stdout=PIPE, stderr=PIPE)
 
-        err = process.stderr.decode()
-        time += float(re.findall(PATTERN, err)[0])
+        out = process.stdout.decode()
+        search = re.search(PATTERN, out)
+        assert search is not None
+
+        time += float(search.group(1))
 
     print(f'- {time / EXECUTIONS:.3f} s')
