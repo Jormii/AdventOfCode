c26a8cd665d6384347c4a05dc5bf764f5295fea1
Jorge Lopez <lopeznatalj@gmail.com>
2024-12-03 15:41:41 +0100

    [2024 Day 3] Part 2

---
+++ 2024/03/part2.py
@@ -0,0 +1,57 @@
+import os
+import re
+import sys
+import time
+
+SOLUTION = 103811193
+INPUT_FILE = os.path.join(os.path.split(__file__)[0], 'input.txt')
+
+
+def main() -> int:
+    t = time.perf_counter()
+
+    DO_REGEX = r'do\(\)'
+    DONT_REGEX = r'don\'t\(\)'
+    MUL_REGEX = r'mul\((\d{1,3}),(\d{1,3})\)'
+
+    DO_PATTERN = re.compile(DO_REGEX)
+    DONT_PATTERN = re.compile(DONT_REGEX)
+    MUL_PATTERN = re.compile(MUL_REGEX)
+
+    total = 0
+    with open(INPUT_FILE) as fd:
+        enabled = True
+
+        for line in fd.readlines():
+            enable_insts = [(m.span()[0], True)
+                            for m in DO_PATTERN.finditer(line)]
+            enable_insts.extend([(m.span()[0], False)
+                                for m in DONT_PATTERN.finditer(line)])
+
+            enable_insts.sort()
+            enable_insts.append((sys.maxsize, enable_insts[-1][1]))
+
+            enable_insts_idx = 0
+            for mul_inst in MUL_PATTERN.finditer(line):
+                begin = mul_inst.span()[0]
+                while enable_insts[enable_insts_idx][0] < begin:
+                    enabled = enable_insts[enable_insts_idx][1]
+                    enable_insts_idx += 1
+
+                if enabled:
+                    left = int(mul_inst.group(1))
+                    right = int(mul_inst.group(2))
+
+                    total += left * right
+
+    tf = time.perf_counter()
+
+    success = total == SOLUTION
+    print(tf - t, file=sys.stderr)
+    print(f'Solution: {total} ({success})')
+
+    return 0 if success else 1
+
+
+if __name__ == '__main__':
+    exit(main())
