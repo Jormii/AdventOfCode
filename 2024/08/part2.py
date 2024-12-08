d2042121f1346d331dd53204dab9a92502915ee8
Jorge Lopez <lopeznatalj@gmail.com>
2024-12-08 11:26:07 +0100

    [2024 Day 8] Part 2

---
+++ 2024/08/part2.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import time
+from typing import Dict, List, Set, Tuple
+
+BIGBOY = False
+
+if not BIGBOY:
+    SOLUTION = 1200
+    INPUT_FILE = os.path.join(os.path.split(__file__)[0], 'input.txt')
+else:
+    SOLUTION = -1
+    INPUT_FILE = os.path.join(os.path.split(__file__)[0], 'bigboy.txt')
+
+PointT = Tuple[int, int]
+
+
+def main() -> int:
+    t = time.perf_counter()
+
+    antinodes: Set[PointT] = set()
+    antennas: Dict[str, List[PointT]] = {}
+    with open(INPUT_FILE) as fd:
+        for r, line in enumerate(fd.readlines()):
+            for c, char in enumerate(line):
+                if char == '.' or char == '\n':
+                    continue
+
+                antinodes.add((r, c))
+
+                if char not in antennas:
+                    antennas[char] = [(r, c)]
+                else:
+                    antennas[char].append((r, c))
+
+    rows = r + 1
+    cols = c
+
+    for char, char_antennas in antennas.items():
+        for i, (ri, ci) in enumerate(char_antennas):
+            for j in range(i + 1, len(char_antennas)):
+                rj, cj = char_antennas[j]
+
+                vr = rj - ri
+                vc = cj - ci
+
+                r, c = ri - vr, ci - vc
+                while 0 <= r < rows and 0 <= c < cols:
+                    antinodes.add((r, c))
+                    r, c = r - vr, c - vc
+
+                r, c = rj + vr, cj + vc
+                while 0 <= r < rows and 0 <= c < cols:
+                    antinodes.add((r, c))
+                    r, c = r + vr, c + vc
+
+    locations_count = len(antinodes)
+
+    tf = time.perf_counter()
+
+    success = locations_count == SOLUTION
+    print(tf - t, file=sys.stderr)
+    print(f'Solution: {locations_count} ({success})')
+
+    return 0 if success else 1
+
+
+if __name__ == '__main__':
+    exit(main())
